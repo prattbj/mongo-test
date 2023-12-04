cc456bc51c6be8cfef21377c734db9d05b18e6c9
Benjamin Pratt <104381242+prattbj@users.noreply.github.com>
2023-12-04 10:17:24 -0700

    Restructured the code
    moved each inner try - catch to its own method so that not everything is in main(). Added delete function.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
@@ -34,49 +35,86 @@ public class Main {
         try (MongoClient mongoClient = MongoClients.create(settings)) {
             MongoDatabase database = mongoClient.getDatabase("Users");
             MongoCollection<Document> collection = database.getCollection("Accounts");
-            try {
-                // Send a ping to confirm a successful connection
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException me) {
-                System.err.println(me);
-            }
-            try {
-                // Inserts a sample document describing a movie into the collection
-                InsertOneResult result = collection.insertOne(new Document()
-                        .append("_id", new ObjectId())
-                        .append("email", "nip20001@byui.edu")
-                        .append("role", "admin")
-                        .append("first_name", "Tyson")
-                        .append("last_name", "Mergel")
-                        .append("bio", "free text")
-                        .append("technology", Arrays.asList("Python", "Java", "MangoDB"))
-                        .append("Social", Arrays.asList("Facebook", "Java", "MangoDB"))
-                        .append("messengers", Arrays.asList("Type", "discord", "Link")));
 
-                // Prints the ID of the inserted document
-                System.out.println("Success! Inserted document id: " + result.getInsertedId());
+            // Send a ping to confirm a successful connection
+            Bson command = new BsonDocument("ping", new BsonInt64(1));
+            database.runCommand(command);
+            System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
 
-                // Prints a message if any exceptions occur during the operation
-            } catch (MongoException me) {
-                System.err.println("Unable to insert due to an error: " + me);
-            }
+            //Create document to insert
+            Document toInsert = new Document()
+                    .append("_id", new ObjectId())
+                    .append("email", "nip20001@byui.edu")
+                    .append("role", "admin")
+                    .append("first_name", "Tyson")
+                    .append("last_name", "Mergel")
+                    .append("bio", "free text")
+                    .append("technology", Arrays.asList("Python", "Java", "MangoDB"))
+                    .append("Social", Arrays.asList("Facebook", "Java", "MangoDB"))
+                    .append("messengers", Arrays.asList("Type", "discord", "Link"));
+
+            insert(collection, toInsert);
+
+            // Create things to update
             Document query = new Document().append("first_name",  "Tyson");
             Bson updates = Updates.combine(
                     Updates.set("last_name", "Nipges-Mergel"),
                     Updates.addToSet("Bio", "Here to have fun"),
                     Updates.currentTimestamp("lastUpdated"));
             UpdateOptions options = new UpdateOptions().upsert(true);
-            try {
-                UpdateResult result = collection.updateOne(query, updates, options);
-                System.out.println("Modified document count: " + result.getModifiedCount());
-                System.out.println("Upserted id: " + result.getUpsertedId()); // only contains a value when an upsert is performed
-            } catch (MongoException me) {
-                System.err.println("Unable to update due to an error: " + me);
-            }
 
+            update(collection, query, updates, options);
+
+            //Create document to delete
+            Document toDelete = new Document("first_name", "Tyson");
+
+            delete(collection, toDelete);
+
+
+        } catch(MongoException me)
+        {
+            System.err.println("Unable to connect to database: " + me);
         }
+    }
+    // Insert a document into a collection
+    public static void insert(MongoCollection<Document> collection, Document toInsert)
+    {
+        try {
+            // Inserts a sample document describing a movie into the collection
+            InsertOneResult result = collection.insertOne(toInsert);
 
+            // Prints the ID of the inserted document
+            System.out.println("Success! Inserted document id: " + result.getInsertedId());
+
+            // Prints a message if any exceptions occur during the operation
+        } catch (MongoException me) {
+            System.err.println("Unable to insert due to an error: " + me);
+        }
     }
+
+    //Update a document
+    public static void update(MongoCollection<Document> collection, Document query, Bson updates, UpdateOptions options)
+    {
+        try {
+            UpdateResult result = collection.updateOne(query, updates, options);
+            System.out.println("Modified document count: " + result.getModifiedCount());
+            System.out.println("Upserted id: " + result.getUpsertedId()); // only contains a value when an upsert is performed
+        } catch (MongoException me) {
+            System.err.println("Unable to update due to an error: " + me);
+        }
     }
+
+    //Delete a document
+    public static void delete(MongoCollection<Document> collection, Document toDelete)
+    {
+        //Note that no exception will be thrown if there isn't anything to delete.
+        try {
+
+            DeleteResult result = collection.deleteOne(toDelete);
+            System.out.println("Deleted document count: " + result.getDeletedCount());
+
+        } catch (MongoException me){
+            System.err.println("Unable to delete item: " + me);
+        }
+    }
+}
